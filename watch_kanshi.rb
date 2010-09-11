77c2bf24b61d320ac1bd2626b8a275cd34461ac3
sanemat <o.gata.ken@gmail.com>
2010-09-11 14:25:44 +0900

    add setup method

--- watch_kanshi.rb
+++ watch_kanshi.rb
@@ -1,13 +1,21 @@
 # coding: utf-8
-def number_of_orders
-  1001
-end
-
 def event(message)
   puts "alert message #{message}" if yield
 end
 
-event "注文が殺到" do
-  recent_orders = number_of_orders
-  recent_orders > 1000
+def setup(&block)
+  yield
 end
+def monthly_sales
+  90
+end
+setup do
+  @target_sales = 100
+end
+event "月間売上が驚くほど高い" do
+  monthly_sales > @target_sales
+end
+event "月間売上が底なしに低い" do
+  monthly_sales < @target_sales
+end
+
